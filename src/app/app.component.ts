f4d73b91d09f7c0bb987822d1130375de627fca8
PranjalAlbhar <34841836+PranjalAlbhar@users.noreply.github.com>
2018-08-08 19:09:00 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,32 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div style="text-align:center">
+  <h1>
+   {{ title }}!
+  </h1>
+  <h2>
+    {{title2}}
+    </h2>
+</div>
+
+
+  `,
+  styles: [`
+    h1
+    {
+      color:red;
+
+    }
+    h2
+    {
+      color:green;
+    }
+  
+  `]
 })
 export class AppComponent {
- public  title = 'Marvellous Infosystems';
-  public title2="Educating for better tommorow";
+  title = 'Marvellous';
+  title2="Inline"
 }
